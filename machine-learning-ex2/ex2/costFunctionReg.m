7e53ccb13f188d0f9fc1c9a222074862b3cba31e
herishbrith <harshbh@hotmail.com>
2017-09-16 00:39:56 +0530

    regularization for sigmoid function added to ex3

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -18,10 +18,16 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 [normal_cost, normal_grad] = costFunction(theta, X, y);
-J = normal_cost + (lambda / (2 * m)) * sum(theta(2:end) .^ 2);
-grad = normal_grad' + (theta .* (lambda / m));
-% grad = bsxfun(@plus, normal_grad', (theta .* (lambda / m))); % One way to add two mat element-wise
+cost_regularization = (lambda / (2 * m)) * sum(theta(2:end) .^ 2);
+J = normal_cost + cost_regularization;
+
+% T done in order to add with normal_grad
+% Also, we don't require first element for regularization
+% So we set the value of first element right away
 grad(1) = normal_grad(1);
+grad_regularization = (theta(2:end) .* (lambda / m))';
+grad(2:end) = normal_grad(2:end) + grad_regularization;
+% grad = bsxfun(@plus, normal_grad, grad_regularization); % One way to add two mat element-wise
 
 % =============================================================
 
