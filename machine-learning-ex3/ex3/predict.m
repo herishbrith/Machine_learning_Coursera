732202dbbf080639550491d668107c40ba8e1657
herishbrith <harshbh@hotmail.com>
2017-09-16 03:13:55 +0530

    prediction using nn provided with theta values

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -10,6 +10,8 @@ num_labels = size(Theta2, 1);
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
+X = [ones(m, 1), X];
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
 %               your learned neural network. You should set p to a 
@@ -21,15 +23,14 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
-
-
-
-
+% Calculate input to hidden layer neurons, a_hidden
+for row = 1:m
+	a_hidden = [1; sigmoid(sum(bsxfun(@times, Theta1, X(row,:)), 2))];
+	a_output = sigmoid(sum(bsxfun(@times, Theta2, a_hidden'), 2));
+	[max_value max_index] = max(a_output);
+	p(row) = max_index;
+end
 
 % =========================================================================
-
 
 end
